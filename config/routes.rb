4fef6346219746cec455a5a2dca1ea091f73bccf
Olivier Girardot <olive_girardot@hotmail.com>
2023-02-20 15:34:15 +0100

    Revert "added reviews create route"
    This reverts commit 2e74a6f4165ba2c490ffdbda3b48f7d01dab1513.

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
-  root to: "restaurants#index"
-  resources :restaurants, only: [:index, :show] do
-    resources :reviews, only: :create
-  end
+  root to: "pages#home"
+  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+
+  # Defines the root path route ("/")
+  # root "articles#index"
 end
